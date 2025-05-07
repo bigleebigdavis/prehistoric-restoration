545e71902c75a51c5abd5168e5807d9579ea7a11
Lee Davis <lgdavisdc@gmail.com>
2025-05-07 11:00:14 -0400

    Update PrehistoricRestoration.java
    Changing MOD_ID Format

--- src/main/java/com/blbd/prehistoricrestoration/PrehistoricRestoration.java
+++ src/main/java/com/blbd/prehistoricrestoration/PrehistoricRestoration.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PrehistoricRestoration implements ModInitializer {
-	public static final String MOD_ID = "prehistoric-restoration";
+	public static final String MOD_ID = "prehistoricrestoration";
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
